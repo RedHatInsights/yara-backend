cff8b44844cfd6055b1815f408704b8a2ee87034
Dan Kuc <dkuc@redhat.com>
2020-12-04 16:00:46 -0500

    Fix rawrule in dev seed

--- src/scripts/import_local.ts
+++ src/scripts/import_local.ts
@@ -1,6 +1,9 @@
+import * as fs from "fs";
+import {raw} from "express";
+
 const util = require('util');
 const {Client} = require('pg')
-const client = new Client({connectionString: 'postgres://postgres:postgres@localhost:5434/myapp'})
+const client = new Client({connectionString: 'postgres://postgres:postgres@localhost:5434/yara'})
 const exec = util.promisify(require('child_process').exec);
 
 const dir = '/home/dkuc/code/rules'
@@ -9,13 +12,14 @@ const dir = '/home/dkuc/code/rules'
 async function startImport() {
     await client.connect()
 
-    const {stdout} = await exec(`find ${dir} -type f -name "*.yar"`);
-    for (const file of stdout.trim().split('\n')) {
+    const {stdout: yaraFiles} = await exec(`find ${dir} -type f -name "*.yar"`);
+    for (const file of yaraFiles.trim().split('\n')) {
         console.log(`Attempting to insert ${file}`);
         try {
             const {stdout} = await exec(`plyara ${file}`);
             //console.log(file)
             let parsedRules = JSON.parse(stdout);
+            const rawRule = fs.readFileSync(file);
             if (parsedRules.length !== 1) {
                 console.log(`skipping ${file} because it does not contain a single rule`);
                 continue;
@@ -27,7 +31,7 @@ async function startImport() {
             const metadata = Object.assign.apply(Object, parsedRule.metadata);
 
             const res = await client.query('INSERT INTO rule (name, tags, metadata, raw_rule) VALUES ($1,$2,$3,$4)',
-                [parsedRule.rule_name, parsedRule.tags, metadata, stdout])
+                [parsedRule.rule_name, parsedRule.tags, metadata, rawRule])
 
 
         } catch (e) {
